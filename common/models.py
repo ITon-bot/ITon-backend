8e49f1f8f4a12f5dfdb35ed618f3f4593848f7c9
Quatry <stop.mean1@gmail.com>
2025-01-05 03:39:31 +0300

    Добавлена модель Location

--- common/models.py
+++ common/models.py
@@ -13,3 +13,15 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField(blank=True, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
