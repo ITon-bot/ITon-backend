5db76004f19ebbee859a47f654b978417f697273
Quatry <stop.mean1@gmail.com>
2025-02-06 21:39:56 +0300

    Сериализаторы для вакансий

---
+++ vacancies/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+
+from common.serializers import SkillSerializer, SpecializationSerializer, LocationSerializer
+from vacancies.models import Vacancy
+
+
+class VacancySerializer(serializers.ModelSerializer):
+    specialization = SpecializationSerializer(read_only=True)
+    location = LocationSerializer(read_only=True)
+    skills = SkillSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Vacancy
+        fields = '__all__'
+
+
+class VacancyFeedSerializer(serializers.ModelSerializer):
+    match_score = serializers.SerializerMethodField()
+    skills = SkillSerializer(many=True, read_only=True)
+    specialization = SpecializationSerializer()
+
+    class Meta:
+        model = Vacancy
+        fields = [
+            'id',
+            'name',
+            'company_name',
+            'type',
+            'job_format',
+            'experience',
+            'min_payment',
+            'max_payment',
+            'specialization',
+            'skills',
+            'location',
+            'match_score',
+        ]
+
+    def get_match_score(self, obj):
+        """
+        Возвращает значение match_score, которое было аннотировано в QuerySet.
+        """
+        if hasattr(obj, 'match_score'):
+            return obj.match_score
+        return None
