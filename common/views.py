6c8f0b37cc261ed578a8f4ed0d7939b91132486e
Quatry <stop.mean1@gmail.com>
2025-02-06 21:57:54 +0300

    Вьюсеты для специализаций и скиллов

--- common/views.py
+++ common/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from common.models import Specialization, Skill
+from common.serializers import SpecializationSerializer, SkillSerializer
+
+
+class SpecializationViewSet(mixins.CreateModelMixin,
+                            mixins.RetrieveModelMixin,
+                            mixins.UpdateModelMixin,
+                            mixins.ListModelMixin,
+                            mixins.DestroyModelMixin,
+                            GenericViewSet):
+    serializer_class = SpecializationSerializer
+    queryset = Specialization.objects.all()
+
+
+class SkillViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.ListModelMixin,
+                   mixins.DestroyModelMixin,
+                   GenericViewSet):
+    serializer_class = SkillSerializer
+    queryset = Skill.objects.all()
