6016e76f3acbb317372be6474a20de2ee239ea2b
Quatry <stop.mean1@gmail.com>
2025-02-10 19:59:52 +0300

    В админ панель добавлены Vacancy и VacancyResponse

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from vacancies.models import Vacancy, VacancyResponse
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(VacancyResponse)
+class VacancyResponseAdmin(admin.ModelAdmin):
+    pass
