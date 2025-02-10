d1008e79ce14fa0e27bda16743c0cfb4a41e98f6
Quatry <stop.mean1@gmail.com>
2025-02-10 20:19:35 +0300

    Добавлены сериализаторы SpecializationSerializer, SkillSerializer

--- common/serializers.py
+++ common/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from common.models import Location
+from common.models import Location, Specialization, Skill
 
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = ['id', 'name', 'address', 'latitude', 'longitude']
+
+
+class SpecializationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Specialization
+        fields = ['name']
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = ['name']
