64526c3641d633cb4609d1407dd2707c7666e56e
Quatry <stop.mean1@gmail.com>
2025-02-10 20:02:46 +0300

    Создан эндпоинт responses для получения списка откликов + VacancyResponseViewSet

--- vacancies/views.py
+++ vacancies/views.py
@@ -1,11 +1,12 @@
 from django.db.models import F
-from rest_framework import mixins
+from rest_framework import mixins, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from vacancies.models import Vacancy
-from vacancies.serializers import VacancySerializer, VacancyFeedSerializer
+from vacancies.models import Vacancy, VacancyResponse
+from vacancies.serializers import (VacancyFeedSerializer, VacancyCreateSerializer,
+                                   VacancyResponseSerializer, VacancyResponseStatusUpdateSerializer)
 
 from vacancies.services import annotate_match_score
 
@@ -13,14 +14,12 @@ from vacancies.services import annotate_match_score
 class VacancyViewSet(mixins.CreateModelMixin,
                      mixins.RetrieveModelMixin,
                      mixins.UpdateModelMixin,
-                     mixins.ListModelMixin,
                      mixins.DestroyModelMixin,
                      GenericViewSet):
-    serializer_class = VacancySerializer
+    serializer_class = VacancyCreateSerializer
     queryset = Vacancy.objects.all()
 
     def retrieve(self, request, pk=None, *args, **kwargs):
-
         instance = Vacancy.objects.select_related('creator', 'location', 'specialization') \
             .prefetch_related('skills') \
             .get(id=pk)
@@ -64,3 +63,49 @@ class VacancyViewSet(mixins.CreateModelMixin,
             context={"request": request}
         )
         return self.get_paginated_response(serializer.data) if page else Response(serializer.data)
+
+    @action(detail=True, methods=['GET'], url_path='responses')
+    def responses(self, request, pk=None):
+        vacancy = self.get_object()
+
+        responses_qs = vacancy.responses.all().order_by('is_viewed', '-created_at')
+        responses_list = list(responses_qs)
+
+        serializer = VacancyResponseSerializer(responses_list, many=True)
+
+        vacancy.responses.filter(is_viewed=False).update(is_viewed=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class VacancyResponseViewSet(mixins.CreateModelMixin,
+                             mixins.RetrieveModelMixin,
+                             mixins.UpdateModelMixin,
+                             GenericViewSet):
+    queryset = VacancyResponse.objects.all()
+    serializer_class = VacancyResponseSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['update', 'partial_update']:
+            instance = self.get_object()
+            if self.request.user == instance.vacancy.creator:
+                return VacancyResponseStatusUpdateSerializer
+        return super().get_serializer_class()
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if self.request.user != instance.vacancy.creator:
+            return Response(
+                {'detail': 'Изменять отклик может только создатель вакансии.'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if self.request.user != instance.vacancy.creator:
+            return Response(
+                {'detail': 'Изменять отклик может только создатель вакансии.'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        return super().partial_update(request, *args, **kwargs)
