c9417a4aa12c67acf66c40601923d984350297af
Quatry <stop.mean1@gmail.com>
2025-02-10 20:04:42 +0300

    Убран констрейнт с View

--- views/models.py
+++ views/models.py
@@ -3,12 +3,10 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from users.models import User
 
+
 class View(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
     timestamp = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('user', 'content_type', 'object_id')
