fe97ee9a15954a2c3f244a6f6efdc62083ac5121
Quatry <stop.mean1@gmail.com>
2025-03-31 02:25:34 +0300

    Добавлен эндпоинт для смены языка + list методы для получения Skill и Specialization с кэшированием

--- common/views.py
+++ common/views.py
@@ -1,11 +1,18 @@
-from rest_framework import mixins
+from django.conf.global_settings import LANGUAGE_COOKIE_NAME
+from django.core.cache import cache
+from rest_framework import mixins, status
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
 from common.models import Specialization, Skill, Report
-from common.serializers import SpecializationSerializer, SkillSerializer, ReportSerializer, ReportAdminSerializer
+from common.serializers import SpecializationSerializer, SkillSerializer, ReportSerializer, ReportAdminSerializer, \
+    LanguageSerializer
 from common.services import perform_update_and_notify
+from core.settings import LANGUAGES, CACHE_TTL
 from vacancies.services import send_report_closed_notification
+from django.utils.translation import gettext as _, activate
 
 
 class SpecializationViewSet(mixins.CreateModelMixin,
@@ -20,6 +27,17 @@ class SpecializationViewSet(mixins.CreateModelMixin,
     def get_queryset(self):
         return self.queryset
 
+    def list(self, request):
+        cached_data = cache.get('all_specializations')
+        if cached_data:
+            return Response(cached_data)
+
+        queryset = Specialization.objects.all().order_by('name')
+        serializer = SpecializationSerializer(queryset, many=True)
+        data = serializer.data
+        cache.set('all_specializations', data, timeout=CACHE_TTL)
+        return Response(data)
+
 
 class SkillViewSet(mixins.CreateModelMixin,
                    mixins.RetrieveModelMixin,
@@ -32,6 +50,17 @@ class SkillViewSet(mixins.CreateModelMixin,
 
     def get_queryset(self):
         return self.queryset
+
+    def list(self, request):
+        cached_data = cache.get('all_skills')
+        if cached_data:
+            return Response(cached_data)
+
+        queryset = Skill.objects.all().order_by('name')
+        serializer = SkillSerializer(queryset, many=True)
+        data = serializer.data
+        cache.set('all_skills', data, timeout=CACHE_TTL)
+        return Response(data)
 
 
 class ReportViewSet(mixins.CreateModelMixin,
@@ -80,3 +109,29 @@ class ReportAdminViewSet(mixins.CreateModelMixin,
             field_name='is_resolved',
             notification_func=send_report_closed_notification
         )
+
+
+class SetLanguageView(APIView):
+    """
+    Эндпоинт для смены языка.
+    """
+
+    def post(self, request, *args, **kwargs):
+        serializer = LanguageSerializer(data=request.data)
+        if serializer.is_valid():
+            lang_code = serializer.validated_data['language']
+
+            if lang_code not in dict(LANGUAGES):
+                return Response({"error": _("Invalid language code")}, status=status.HTTP_400_BAD_REQUEST)
+
+            activate(lang_code)
+
+            if hasattr(request, 'session'):
+                request.session['django_language'] = lang_code
+
+            response = Response({"detail": _("Language has been changed.")}, status=status.HTTP_200_OK)
+            response.set_cookie(LANGUAGE_COOKIE_NAME, lang_code, max_age=31536000)
+
+            return response
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
